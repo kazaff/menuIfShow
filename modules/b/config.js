7fbea25bbe1664c9d2db9641683ccc182bac1148
kazaff <edisondik@gmail.com>
2016-04-20 09:27:55 +0800

    增加全局配置文件

--- modules/b/config.js
+++ modules/b/config.js
@@ -1,7 +1,8 @@
+var initConfig = require("../../config.js");	//加载配置文件
 module.exports = [{
 	link_id: 3,	//前端操作编号，用来获取指定操作数据时使用，不允许冲突
 	key: "menu_b",	//操作名称key，用来从语言配置中获取显示名称使用
-	link_url: "b/b.html", 	//对应链接指向的url
+	link_url:initConfig.domain+"b/b.html", 	//对应链接指向的url
 	order: 2,	//显示时使用的排序
 	isDir: false,	//表示是否未顶级菜单
 	dir_id: 1,	//表示所属的顶级菜单，若该操作既不是顶级菜单，也不属于任何顶级菜单，则表示该操作不是菜单，而是页面中的操作链接
